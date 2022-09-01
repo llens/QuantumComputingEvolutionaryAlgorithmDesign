ecf2fb9deef0323bbc7c43382837f3b0bbbfe520
Tom Snell <tstomsnell@gmail.com>
2022-08-31 19:17:39 -0600

    compatibility fix and update readme

--- quantum_computing_algorithm_building_script.py
+++ quantum_computing_algorithm_building_script.py
@@ -1,7 +1,12 @@
+import array
+from deap import creator, base
+
 from Config import Config
 from EvolutionaryAlgorithm import EvolutionaryAlgorithm
 from target_generation import setup_example_problem, ExampleType
 
+creator.create("FitnessMax", base.Fitness, weights=(1.0,))
+creator.create("Individual", array.array, typecode='b', fitness=creator.FitnessMax)
 
 if __name__ == "__main__":
     config = Config()
